de3b6b8590442cd127b395383d28528303f252c5
epsilon <martin.olivier1997@gmail.com>
2023-09-12 17:58:50 +0200

    added image autosaving

--- fractal.py
+++ fractal.py
@@ -7,6 +7,7 @@ from pandas.errors import EmptyDataError
 import logging
 from os import listdir, mkdir, getcwd, remove
 from os.path import isfile, join
+import time
 
 logger = logging.getLogger("__name__")
 
@@ -30,7 +31,10 @@ class FractalManager():
     def _load(self, filename):
         return np.load(join(self.checkpoint_output_dir, filename))
 
-    def save(self):
+    def save_checkpoint(self):
+        logger.info(f'Saving last checkpoint: {join(self.fractal_output_dir, self.checkpoint_filename)}')
+        return np.save(join(self.checkpoint_output_dir, self.checkpoint_filename), self.last_checkpoint)
+
         logger.info(f'Saving last checkpoint: {join(self.fractal_output_dir, self.checkpoint_filename)}')
         return np.save(join(self.checkpoint_output_dir, self.checkpoint_filename), self.last_checkpoint)
 
@@ -70,15 +74,17 @@ class FractalManager():
         for data in files:
            self._compute(data)
 
-        self._output_image()
+        self.save_image(self.output_filename)
 
     def _compute(self, histogram):
         self.last_checkpoint = np.add(self.last_checkpoint, histogram)
 
-    def _output_image(self):
-        logger.debug(f'Saving output image at {join(self.fractal_output_dir, self.output_filename)}')
+    def save_image(self, filename = None):
+        if not filename:
+            filename = f'{str(round(time.time()))}.png'
+        logger.debug(f'Saving output image at {join(self.fractal_output_dir, filename)}')
         max_val = np.log(np.max(self.last_checkpoint) + 1)
         out_array = self.smoothing_func(self.last_checkpoint, max_val)
         output_img = Image.fromarray(out_array.astype(np.uint8))
         output_img.convert("L")
-        output_img.save(join(self.fractal_output_dir, self.output_filename))
+        output_img.save(join(self.fractal_output_dir, filename))
--- server.py
+++ server.py
@@ -135,7 +135,8 @@ if __name__ == '__main__':
 
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=fractal_mgr.compute_histograms, trigger="interval", seconds=30)
-    scheduler.add_job(func=fractal_mgr.save, trigger="interval", seconds=30)
+    scheduler.add_job(func=fractal_mgr.save_checkpoint, trigger="interval", seconds=30)
+    scheduler.add_job(func=fractal_mgr.save_image, trigger="interval", seconds=30)
     scheduler.start()
 
     app.run(debug=args.debug, port=8000)
